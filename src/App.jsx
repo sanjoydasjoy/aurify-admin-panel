3ad32769357d6940eba3cedfda34da3da0e2d4c6
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-01-15 23:59:49 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Login from './components/Login'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+
 //export const backendURL = import.meta.env.VITE_BACKEND_URL;
 export const backendURL = import.meta.env.VITE_BACKEND_URL;
 export const currency = '৳'
