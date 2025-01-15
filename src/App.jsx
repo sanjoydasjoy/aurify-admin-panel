561fc1029840403b773c830f0fa267777c073b9b
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-01-15 23:58:52 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,10 @@ import Login from './components/Login'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+//export const backendURL = import.meta.env.VITE_BACKEND_URL;
 export const backendURL = import.meta.env.VITE_BACKEND_URL;
+export const currency = '৳'
+
 
 
 const App = () => {
@@ -33,6 +36,7 @@ const App = () => {
             <Sidebar />
             <div className='w-[70%] mx-auto ml-[max(5vw, 25px) ] my-8 text-gray-600 text-base' >
               <Routes>
+                <Route path="/" element={<List token={token} />} /> {/* Default Route */}
                 <Route path='/add' element={<Add token={token} />} />
                 <Route path='/list' element={<List token={token} />} />
                 <Route path='/orders' element={<Orders token={token} />} />
